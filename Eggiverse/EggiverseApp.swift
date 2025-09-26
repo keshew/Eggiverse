597c7cd201aaedf508db354524e43b3ab091b3e5
keshew <artyom.korotckov2017@yandex.ru>
2025-09-26 10:24:31 +0500

    Initial Commit

---
+++ Eggiverse/EggiverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggiverseApp.swift
+//  Eggiverse
+//
+//  Created by Артём Коротков on 26.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggiverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
